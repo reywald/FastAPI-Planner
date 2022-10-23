5fc27f739c253ca218fd480636dd95627512eea9
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-10-23 11:36:28 +0100

    Chapter 6: Secure the application with OAuth2 and JWT

--- routes/users.py
+++ routes/users.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, HTTPException, status
+from auth.hash_password import HashPassword
 from database.connection import Database
 from models.users import User, UserSigIn
 
 user_router = APIRouter(tags=["User"])
 
 user_database = Database(User)
+hash_password = HashPassword()
 
 
 @user_router.post("/signup")
@@ -13,6 +15,9 @@ async def sign_user_up(user: User) -> dict:
     if user_exist:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                             detail="User with email provided exists already.")
+
+    hashed_password = hash_password.create_hash(user.password)
+    user.password = hashed_password
 
     await user_database.save(user)
     return {"message": "User successfully registered!"}
